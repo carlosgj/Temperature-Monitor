fc08be4a775ad629e298b86befec4f08d06feeea
Carlos Gross Jones <carlosj@jpl.nasa.gov>
2024-01-30 17:37:30 -0800

    Splitting out UI stuff

---
+++ Receiver.X/UI.c
@@ -0,0 +1,192 @@
+#include "UI.h"
+
+uint8_t UI_init(void){
+    uint8_t status = 0;
+    
+    //Set up buttons
+    BUTTON1_TRIS = INPUT;
+    BUTTON2_TRIS = INPUT;
+    BUTTON3_TRIS = INPUT;
+    BUTTON4_TRIS = INPUT;
+    BUTTON5_TRIS = INPUT;
+    BUTTON6_TRIS = INPUT;
+
+    BUTTON1_WPU = TRUE;
+    BUTTON2_WPU = TRUE;
+    BUTTON3_WPU = TRUE;
+    BUTTON4_WPU = TRUE;
+    BUTTON5_WPU = TRUE;
+    BUTTON6_WPU = TRUE;
+
+    buttonState.all = 0;
+    oldButtonState.all = 0;
+    buttonPressed.all = 0;
+    
+    return status;
+}
+
+void UI_periodic(void){
+    
+}
+
+
+void updateButtons(void) {
+    oldButtonState.all = buttonState.all;
+
+//    buttonState.B1 = !BUTTON1_PORT;
+//    buttonState.B2 = !BUTTON2_PORT;
+//    buttonState.B3 = !BUTTON3_PORT;
+//    buttonState.B4 = !BUTTON4_PORT;
+//    buttonState.B5 = !BUTTON5_PORT;
+//    buttonState.B6 = !BUTTON6_PORT;
+
+    buttonPressed.all = buttonState.all & ~oldButtonState.all;
+}
+
+void handleButtonActions(void) {
+//    //If any button is pressed when asleep, wake up
+//    if (buttonPressed.all != 0 && isSleep) {
+//        setSleep(FALSE);
+//        __delay_ms(250);
+//        return;
+//    }
+//    if (buttonPressed.B1) {
+//        switch (currentDisplayMode) {
+//            case DISP_MODE_HOME:
+//                setGraphMode(GRAPH_MODE_DAY);
+//                break;
+//            case DISP_MODE_UTIL:
+//                //Switch to home
+//                setDisplayMode(DISP_MODE_HOME);
+//                break;
+//            case DISP_MODE_SETTIME:
+//                //Switch to home
+//                setDisplayMode(DISP_MODE_HOME);
+//                break;
+//        }
+//        return;
+//    }
+//
+//    if (buttonPressed.B2) {
+//        switch (currentDisplayMode) {
+//            case DISP_MODE_HOME:
+//                //Switch to past-week plot
+//                setGraphMode(GRAPH_MODE_WEEK);
+//                break;
+//            case DISP_MODE_UTIL:
+//                //Switch to time-set screen
+//                setDisplayMode(DISP_MODE_SETTIME);
+//                getTime();
+//                pros_seconds.all = seconds_reg.all;
+//                pros_minutes.all = minutes_reg.all;
+//                pros_hours.all = hours_reg.all;
+//                pros_date.all = date_reg.all;
+//                pros_month.all = month_reg.all;
+//                pros_years.all = years_reg.all;
+//                activeTimeChar = ACTIVE_TIME_10YR;
+//                drawProspectiveTime();
+//                break;
+//            case DISP_MODE_SETTIME:
+//                setTime();
+//                setDisplayMode(DISP_MODE_HOME);
+//                break;
+//        }
+//        return;
+//    }
+//
+//    if (buttonPressed.B3) {
+//        switch (currentDisplayMode) {
+//            case DISP_MODE_HOME:
+//                //Switch to past-month plot
+//                setGraphMode(GRAPH_MODE_MONTH);
+//                break;
+//            case DISP_MODE_UTIL:
+//                //Force start RTC
+//                RTCOscRestart();
+//                break;
+//            case DISP_MODE_SETTIME:
+//                //Move cursor left
+//                if (activeTimeChar == 0) {
+//                    activeTimeChar = ACTIVE_TIME_SECOND;
+//                } else {
+//                    activeTimeChar--;
+//                }
+//                drawProspectiveTime();
+//                break;
+//            case DISP_MODE_VERIFY_RESET:
+//                //"No" selected
+//                setDisplayMode(DISP_MODE_HOME);
+//                break;
+//        }
+//        return;
+//    }
+//
+//    if (buttonPressed.B4) {
+//        switch (currentDisplayMode) {
+//            case DISP_MODE_HOME:
+//                //Switch to past-year plot
+//                setGraphMode(GRAPH_MODE_YEAR);
+//                break;
+//            case DISP_MODE_UTIL:
+//                //Reset
+//                RESET();
+//                break;
+//            case DISP_MODE_SETTIME:
+//                //Move cursor right
+//                if (activeTimeChar == ACTIVE_TIME_SECOND) {
+//                    activeTimeChar = 0;
+//                } else {
+//                    activeTimeChar++;
+//                }
+//                drawProspectiveTime();
+//                break;
+//            case DISP_MODE_VERIFY_RESET:
+//                //"Yes" selected
+//                safeMode = FALSE;
+//                //resetEEPROMPageIndex();
+//                RESET();
+//                break;
+//        }
+//        return;
+//    }
+//
+//    if (buttonPressed.B5) {
+//        switch (currentDisplayMode) {
+//            case DISP_MODE_HOME:
+//                //Go to util screen
+//                setDisplayMode(DISP_MODE_UTIL);
+//                break;
+//            case DISP_MODE_UTIL:
+//                break;
+//            case DISP_MODE_SETTIME:
+//                //Increment
+//                incrementActiveChar();
+//                drawProspectiveTime();
+//                break;
+//        }
+//        return;
+//    }
+//
+//    if (buttonPressed.B6) {
+//        switch (currentDisplayMode) {
+//            case DISP_MODE_HOME:
+//                //Turn off screen
+//                setSleep(TRUE);
+//                break;
+//            case DISP_MODE_UTIL:
+//                setDisplayMode(DISP_MODE_VERIFY_RESET);
+//                break;
+//            case DISP_MODE_SETTIME:
+//                //Decrement
+//                decrementActiveChar();
+//                drawProspectiveTime();
+//                break;
+//        }
+//        return;
+//    }
+//
+//    //If a button was pressed, do the action, then delay to ignore bounces
+//    if (buttonPressed.all != 0) {
+//        __delay_ms(300);
+//    }
+}
---
+++ Receiver.X/UI.h
@@ -0,0 +1,29 @@
+#ifndef UI_H
+#define	UI_H
+
+#include "common.h"
+
+union buttonReg{
+    unsigned char all;
+    struct{
+        unsigned B1 :1;
+        unsigned B2 :1;
+        unsigned B3 :1;
+        unsigned B4 :1;
+        unsigned B5 :1;
+        unsigned B6 :1;
+        unsigned RES:2;
+    };
+};
+
+union buttonReg buttonState;
+union buttonReg oldButtonState;
+union buttonReg buttonPressed;
+
+uint8_t UI_init(void);
+void UI_periodic(void);
+void updateButtons(void);
+void handleButtonActions(void);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
